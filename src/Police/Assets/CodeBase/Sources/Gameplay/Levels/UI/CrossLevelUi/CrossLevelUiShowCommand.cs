158095fc2e57d5d5ca94b090f3362193cc971569
hermer29 <kliman.chan@gmail.com>
2023-06-05 22:50:41 +0300

    LT в LA

---
+++ src/Police/Assets/CodeBase/Sources/Gameplay/Levels/UI/CrossLevelUi/CrossLevelUiShowCommand.cs
@@ -0,0 +1,30 @@
+﻿using Gameplay.Levels.Services.LevelsTracking;
+
+namespace Gameplay.Levels.UI.CrossLevelUi
+{
+    public class CrossLevelUiShowCommand
+    {
+        private readonly CrossLevelUi _crossLevelUi;
+        private readonly LevelService _level;
+
+        public CrossLevelUiShowCommand(CrossLevelUi crossLevelUi, LevelService level)
+        {
+            _crossLevelUi = crossLevelUi;
+            _level = level;
+        }
+
+        public void Show()
+        {
+            if (_level.Level < 4)
+            {
+                
+            }
+            else if (_level.Level < 3)
+            {
+                
+            }
+        }
+        
+        
+    }
+}
